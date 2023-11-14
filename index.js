13fa3e3b55eaa2b1177c1855f67d16c716b784c3
nehakhan77 <nehakhann71@gmail.com>
2023-11-13 19:41:30 -0500

    Added error-handling middleware function

--- index.js
+++ index.js
@@ -79,11 +79,11 @@ app.get("/", (req, res) => {
   res.send(responseText);
 });
 
-app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
-});
-
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
+});
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
 });
