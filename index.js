5cc11df5aef3f6e367b78b259a4e673286799b4c
nehakhan77 <nehakhann71@gmail.com>
2023-12-18 14:26:59 -0500

    Updates

--- index.js
+++ index.js
@@ -299,8 +299,7 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something broke!");
 });
 
-//Listening for Request
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log("Listening on Port" + port);
+app.listen(port, "0.0.0.0.", () => {
+  console.log("Listening on Port " + port);
 });
