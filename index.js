ea123b1cbc4620f7efb58cf5a3384942096663bd
nehakhan77 <nehakhann71@gmail.com>
2024-01-08 16:09:52 -0500

    Removed passport.authenticate to movies endpoint

--- index.js
+++ index.js
@@ -99,16 +99,19 @@ app.get(
 );
 
 // READ movie list
-app.get("/movies", async (req, res) => {
-  await Movies.find()
-    .then((movies) => {
-      res.status(201).json(movies);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.status(500).send("Error: " + err);
-    });
-});
+app.get(
+  "/movies",
+  async (req, res) => {
+    await Movies.find()
+      .then((movies) => {
+        res.status(201).json(movies);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send("Error: " + err);
+      });
+  }
+);
 
 //READ movie list by movie title
 app.get(
