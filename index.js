909912e4cc5816c0494974f47644c2bae739e734
Neha Khan <nehakhann71@gmail.com>
2024-04-30 12:24:10 -0400

    update allowed origins CORS policy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { check, validationResult } = require("express-validator");
 const app = express();
 
 //Import CORS
-let allowedOrigins = ["http://localhost:8080", "http://testsite.com"];
+let allowedOrigins = ["http://localhost:8080", "http://testsite.com","http://localhost:4200" ];
 //allow specific set of origins to access your API
 app.use(
   cors({
